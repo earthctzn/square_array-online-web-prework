81c7be27d52e3afc284b267e4b1a4b1dbeb75124
Caleb Einarr <earthctzn1@gmail.com>
2019-04-07 02:40:10 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,14 @@
-
+def square_array(array)
+  arr = []
+  array.each { |i| arr << i ** 2 }
+  arr
+end
 
 # Advanced
 
 def square_array(array)
   Enumerator.new do |y|
-    array.collect { |e| y << e ** 2 }    
+    array.each { |e| y << e ** 2 }    
   end
  .take(array.length)
 end
